50951cec40ae1e0fc4175a7379d69cce5e002cb6
Steve Chao <22403555+stchao@users.noreply.github.com>
2024-01-08 17:43:03 -0500

    Add serilog and configurations

---
+++ Bitwarden-Backup/Extensions/ServiceCollectionExtension.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Serilog;
+
+namespace Bitwarden_Backup.Extensions
+{
+    internal static class ServiceCollectionExtension
+    {
+        internal static IServiceCollection ConfigureLogAndServices(this IServiceCollection services)
+        {
+            var configuration = new ConfigurationBuilder()             
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .Build();
+
+            Log.Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(configuration)
+                .CreateLogger();
+
+            services.AddSingleton<IConfiguration>(configuration)
+                .AddLogging(configure => configure.AddSerilog());
+
+            return services;
+        }
+    }
+}
--- Bitwarden-Backup/Program.cs
+++ Bitwarden-Backup/Program.cs
@@ -1,9 +1,16 @@
-﻿namespace Bitwarden_Backup
+﻿using Bitwarden_Backup.Extensions;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Bitwarden_Backup
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            var serviceProvider = new ServiceCollection()
+                .ConfigureLogAndServices()
+                .BuildServiceProvider();
+
             Console.WriteLine("Hello, World!");
         }
     }
